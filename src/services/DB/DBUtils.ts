924e348c5de36ad8e24e26255dcd65468d9d1631
bart <bartek.bober.koder@gmail.com>
2021-11-03 22:08:34 +0100

    add checking memory

--- src/services/DB/DBUtils.ts
+++ src/services/DB/DBUtils.ts
@@ -5,6 +5,14 @@ class DBUtils {
   DBExist = () => !!localStorage.getItem(DB_NAME)
   initDB = () => localStorage.setItem(DB_NAME, JSON.stringify(DB_SCHEMA))
   getDB = () => (this.DBExist() ? localStorage.getItem(DB_NAME) : null)
+  getDbSpace = () => {
+    const data = this.getDB()
+    const memoryUsed =
+      data && Number(((data.length * 16) / (8 * 1024)).toFixed(2))
+    const spaceLeft = data && `${memoryUsed ? 5120 - memoryUsed : '5MB'}`
+
+    return { memoryUsed, spaceLeft }
+  }
 }
 
 export default DBUtils
